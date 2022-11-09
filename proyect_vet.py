b82159b3d27d0dc764819d5eb4de91ae2c745dde
kenneth aldair espino molina <kenneth-molina899@outlook.es>
2022-11-09 17:28:56 -0500

    eliminando_archivo

--- proyect_vet.py
+++ proyect_vet.py
@@ -29,3 +29,25 @@ def menu():
     if
     elif opcion==2:
         modicar(datos)
+def carga():
+    datos={}
+    continua="s"
+    while continua=="s":
+        legajos=int(input("ingrese numero de legajo"))
+        
+        nombre=input("ingrese nombre del empleado")
+        profesion=input("ingrese puesto /area/profesion")
+        sueldo=int(input("ingrese sueldo del empleado"))
+        datos[legajos]=[nombre,profesion,sueldo]
+        continua=input("desea agregar otro empleado?")
+        if continua!="s":
+            menu()      
+    return(datos)
+   
+
+
+def modicar(datos):
+    legajo=int(input("ingrese numero de legajo a seleccionar"))
+    if legajo in datos:
+        sueldo=int(input("ingrese el nuevo sueldo"))
+        datos[2]=sueldo
